25cb16ec3bc63be5d93cf1a88bfa120e746afd9a
Yazmin Teran <diosenyteran@gmail.com>
2024-07-10 22:14:13 -0500

    tipo de datos

---
+++ tipos-de-datos.py
@@ -0,0 +1,15 @@
+"string" # se utiliza para tipos de datos texto
+'string'
+"""string"""
+'''string'''
+
+""""Tus datos son nombre: Yazmin   apellido: Teran"""
+
+
+# Tipo de datos numericos
+4#tipo de datos numericos
+4.5#tipo de datos flotantes
+
+#tipos de datos boleanos
+True#True va con T mayuscula
+False#False va con F mayuscula 
