31fb827f5c6554256078de223ef17463cee0fe41
Yazmin Teran <diosenyteran@gmail.com>
2024-07-10 21:44:01 -0500

    bootcamp-de-python

---
+++ prueba.py
@@ -0,0 +1,13 @@
+nombre = True
+bienvenida = f"hola {nombre} feliz noche"
+
+print (bienvenida)
+print("hola"in bienvenida)
+print("betty"not in bienvenida)
+print ("hola"not in bienvenida)
+
+#camelcase
+nombrecompletodeTuProfe = "jheison gakvis"
+
+##snakeCase es la que debemios utilizar en pyhn
+nombre_completo_de_tu_profe = "jheison gakvis"
